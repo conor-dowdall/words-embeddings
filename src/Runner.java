d41056dd5fbf9b6bb859000dc0ce1667fd7d4e98
Conor Dowdall <conordowdallmail@gmail.com>
2024-05-06 16:30:09 +0100

    update

--- src/Runner.java
+++ src/Runner.java
@@ -3,22 +3,14 @@ import ie.atu.sw.embeddings.WordEmbeddings;
 public class Runner {
     public static void main(String[] args) throws Exception {
         WordEmbeddings wordEmbeddings = new WordEmbeddings();
-        try {
-            double[] search = wordEmbeddings.add("woman", wordEmbeddings.subtract("king", "man"));
 
-            System.out.println("\n\nSimilar\n-------------");
-            String[] similarWords = wordEmbeddings.getSimilarWords(search, 10);
-            for (String word : similarWords)
-                System.out.println(word);
+        double[] search = wordEmbeddings.add("woman", wordEmbeddings.subtract("king",
+                "man"));
 
-            System.out.println("\n\nDissimilar\n-------------");
-
-            String[] dissimilarWords = wordEmbeddings.getDissimilarWords(search, 10);
-            for (String word : dissimilarWords)
-                System.out.println(word);
-
-        } catch (Exception e) {
-            System.err.println(e);
-        }
+        System.out.println("\n\nSimilar\n-------------");
+        // changed to cosine distance, which has reversed word rankings!
+        String[] similarWords = wordEmbeddings.getDissimilarWords(search, 10);
+        for (String word : similarWords)
+            System.out.println(word);
     }
 }
