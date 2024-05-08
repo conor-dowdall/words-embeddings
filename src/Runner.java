e6564148f957202c4db0d0042feea6dccd21b028
Conor Dowdall <conordowdallmail@gmail.com>
2024-05-08 22:53:35 +0100

    use new menu system

--- src/Runner.java
+++ src/Runner.java
@@ -5,8 +5,7 @@ import ie.atu.sw.menu.MainMenu;
 
 public class Runner {
     public static void main(String[] args) throws Exception {
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.run();
+        new MainMenu();
 
         // for (MainMenuItem item : MainMenuItem.values()) {
         // System.out.println(item);
