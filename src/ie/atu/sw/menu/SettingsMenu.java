0f7dc7d20bc0a9704f3319945f89699bbddb9674
Conor Dowdall <conordowdallmail@gmail.com>
2024-05-10 21:00:57 +0100

    refactor and fix keepSettingsOpen bug

--- src/ie/atu/sw/menu/SettingsMenu.java
+++ src/ie/atu/sw/menu/SettingsMenu.java
@@ -1,6 +1,8 @@
 package ie.atu.sw.menu;
 
+import java.io.File;
 import java.util.Scanner;
+import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 
 import ie.atu.sw.console.ConsolePrint;
@@ -8,15 +10,20 @@ import ie.atu.sw.embeddings.WordsEmbeddings;
 
 public class SettingsMenu {
 
-    private boolean keepSettingsOpen = true;
     private Scanner inputScanner;
-    private WordsEmbeddings wordsEmbeddings;
-
     private Preferences preferences;
+    private boolean keepSettingsOpen;
+    private WordsEmbeddings wordsEmbeddings;
 
     public SettingsMenu(Scanner inputScanner) {
         this.inputScanner = inputScanner;
         this.preferences = Preferences.userNodeForPackage(SettingsMenu.class);
+    }
+
+    public void launchMenu() throws Exception {
+        SettingsMenuItem.printTitle();
+        keepSettingsOpen = true;
+        printOptionsAndProcessInput();
     }
 
     public WordsEmbeddings getWordsEmbeddings() {
@@ -27,12 +34,27 @@ public class SettingsMenu {
         return this.preferences.getInt("numberOfSimilaritiesToFind", 10);
     }
 
-    public void launchMenu() {
-        SettingsMenuItem.printTitle();
-        printOptionsAndProcessInput();
+    private void setNumberOfSimilaritiesToFind(int number) {
+        this.preferences.putInt("numberOfSimilaritiesToFind", number);
     }
 
-    private void printOptionsAndProcessInput() {
+    public String getDataOutputFileName() {
+        return this.preferences.get("dataOutputFileName", "./out.txt");
+    }
+
+    private void setDataOutputFileName(String fileName) {
+        this.preferences.put("dataOutputFileName", fileName);
+    }
+
+    public boolean getAppendDataOutputFile() {
+        return this.preferences.getBoolean("appendDataOutputFile", true);
+    }
+
+    private void setAppendDataOutputFile(boolean append) {
+        this.preferences.putBoolean("appendDataOutputFile", append);
+    }
+
+    private void printOptionsAndProcessInput() throws Exception {
 
         try {
 
@@ -42,16 +64,18 @@ public class SettingsMenu {
             SettingsMenuItem item = SettingsMenuItem.valueOfKey(input);
 
             switch (item) {
-                case EMBEDDINGS_FILE -> loadNewWordEmbeddings();
-                case OUTPUT_FILE -> specifyNewOutputFile();
-                case CLEAR_OUTPUT_FILE -> System.out.println("TODO");
-                case SIMILARITIES_NUMBER -> preferences.putInt("numberOfSimilaritiesToFind", 2);
+                case EMBEDDINGS_FILE -> loadNewWordsEmbeddingsFile();
+                case SIMILARITIES_NUMBER -> specifySimilarities();
                 case SIMILARITIES_ALGORITHM -> System.out.println("TODO");
                 case ADD_WORD_STATS -> System.out.println("TODO");
+                case OUTPUT_FILE -> specifyNewDataOutputFileName();
+                case TOGGLE_APPEND -> toggleAppendDataOutputFile();
+                case EMPTY_OUTPUT_FILE -> emptyDataOutputFile();
+                case RESET -> resetSettings();
                 case QUIT -> quitSettings();
                 default -> throw new Exception("This should never happen!");
-
             }
+
         } catch (Exception e) {
             ConsolePrint.printError(e.getMessage());
         } finally {
@@ -61,34 +85,124 @@ public class SettingsMenu {
 
     }
 
-    public void loadNewWordEmbeddings() throws Exception {
-        ConsolePrint.printHeading("Load Word-Embeddings File");
+    public void loadNewWordsEmbeddingsFile() throws Exception {
+        ConsolePrint.printHeading("Load Words-Embeddings File");
 
         String wordsEmbeddingsFileName = scanFileName();
 
         this.wordsEmbeddings = new WordsEmbeddings(wordsEmbeddingsFileName);
+
+        ConsolePrint.printInfo("Words-Embeddings file loaded: " + wordsEmbeddingsFileName);
     }
 
-    private void specifyNewOutputFile() throws Exception {
-        ConsolePrint.printHeading("Specify Output-Data File");
+    private void specifySimilarities() throws Exception {
+        ConsolePrint.printHeading("Specify Number of Similarities to Find");
 
-        String outputFileName = scanFileName();
+        int similarities = scanSimilarities();
 
-        System.out.println("You entered: " + outputFileName);
+        setNumberOfSimilaritiesToFind(similarities);
+
+        ConsolePrint.printInfo("Number of Similarities To Find is set to: " + similarities);
     }
 
     private String scanFileName() throws Exception {
         System.out.print("Enter file name: ");
-        String fileName = this.inputScanner.nextLine();
+        String input = this.inputScanner.nextLine();
 
-        if (fileName.equals(""))
+        if (input.equals(""))
             throw new Exception("No file name provided");
 
-        return fileName;
+        return input;
+    }
+
+    private int scanSimilarities() throws Exception {
+        try {
+
+            System.out.print("Enter an integer (greater than 0): ");
+            String input = this.inputScanner.nextLine();
+
+            int number = Integer.parseInt(input);
+
+            if (number < 1)
+                throw new Exception("Number must be greater than 0");
+
+            if (wordsEmbeddings != null) {
+                if (number > this.wordsEmbeddings.getNumberOfWords())
+                    throw new Exception(
+                            "Number to big. Maximum words available = " + this.wordsEmbeddings.getNumberOfWords());
+            } else {
+                ConsolePrint
+                        .printWarning("Make sure to load a words-embeddings file with at least " + number + " word(s)");
+            }
+
+            return number;
+
+        } catch (NumberFormatException e) {
+            throw new Exception("Invalid number. Using previous value: " + getNumberOfSimilaritiesToFind());
+        }
+
+    }
+
+    private void specifyNewDataOutputFileName() throws Exception {
+        ConsolePrint.printHeading("Specify Data-Output File");
+
+        String dataOutputFileName = scanFileName();
+
+        setDataOutputFileName(dataOutputFileName);
+
+        if (getAppendDataOutputFile())
+            ConsolePrint.printInfo("Data output will now be appended to " + getDataOutputFileName());
+        else
+            ConsolePrint.printInfo("Data output will now overwrite " + getDataOutputFileName());
+    }
+
+    private void toggleAppendDataOutputFile() {
+        setAppendDataOutputFile(!getAppendDataOutputFile());
+
+        if (getAppendDataOutputFile())
+            ConsolePrint.printInfo("Data output will now be appended to " + getDataOutputFileName());
+        else
+            ConsolePrint.printInfo("Data output will now overwrite " + getDataOutputFileName());
+    }
+
+    private void emptyDataOutputFile() throws Exception {
+        try {
+
+            File dataOutputFile = new File(getDataOutputFileName());
+
+            if (dataOutputFile.delete())
+                dataOutputFile.createNewFile();
+
+        } catch (Exception e) {
+            throw new Exception("Problem emptying data-output file: " + getDataOutputFileName());
+        }
+
+        ConsolePrint.printInfo("Data-Output file is now empty: " + getDataOutputFileName());
+    }
+
+    private void resetSettings() throws BackingStoreException {
+        this.preferences.clear();
+
+        ConsolePrint.printHeading("Settings reset to default values");
+        printSettings();
+    }
+
+    public void printSettings() {
+
+        String wordsEmbeddingsFileName = this.wordsEmbeddings == null ? "NOT SET" : this.wordsEmbeddings.getFileName();
+        String appendOverwrite = getAppendDataOutputFile() ? "append" : "overwrite";
+
+        ConsolePrint.printInfo("Embeddings File: " + wordsEmbeddingsFileName);
+        ConsolePrint.printInfo("Number of Words to Find: " + getNumberOfSimilaritiesToFind());
+        ConsolePrint.printInfo("Data-Output File: " + getDataOutputFileName());
+        ConsolePrint.printInfo("Append/Overwrite Mode: " + appendOverwrite);
+        System.out.println();
     }
 
     private void quitSettings() {
         keepSettingsOpen = false;
+
+        ConsolePrint.printInfo("Close Settings Menu");
     }
 
 }
