eaf98acecf03899ed495276da24a20edb831d475
Conor Dowdall <conordowdallmail@gmail.com>
2024-05-10 20:59:05 +0100

    add throws exception

--- src/Runner.java
+++ src/Runner.java
@@ -2,7 +2,7 @@
 import ie.atu.sw.menu.MainMenu;
 
 public class Runner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
         new MainMenu().launchMenu();
 
