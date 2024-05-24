daa7bfee67693c7a67067733105045107503491f
Conor Dowdall <conordowdallmail@gmail.com>
2024-05-24 12:12:26 +0100

    add javadoc comments

--- src/ie/atu/sw/util/Array.java
+++ src/ie/atu/sw/util/Array.java
@@ -1,6 +1,18 @@
 package ie.atu.sw.util;
 
+/**
+ * utility class with static methods for finding the indexes of the
+ * minimum-or-maximum values in arrays of doubles
+ */
 public class Array {
+    /**
+     * find the indexes of 'howMany' maximum values of an array
+     * 
+     * @param array   - the array to search
+     * @param howMany - how many results to search for
+     * @return an array of the searched for indexes
+     * @throws Exception if the array length is shorter than 'howMany'
+     */
     public static int[] getMaxValuesIndexes(double[] array, int howMany) throws Exception {
         if (howMany > array.length)
             throw new Exception(
@@ -17,6 +29,14 @@ public class Array {
         return maxValuesIndexes;
     }
 
+    /**
+     * find the index of the maximum value of an array, and set the value at that
+     * index to the lowest value possible, effectively removing it from future
+     * searches
+     * 
+     * @param array - the array to search
+     * @return an array of the searched for indexes
+     */
     public static int getMaxValueIndex(double[] array) {
         int maxValueIndex = 0;
 
@@ -29,6 +49,14 @@ public class Array {
         return maxValueIndex;
     }
 
+    /**
+     * find the indexes of 'howMany' minimum values of an array
+     * 
+     * @param array   - the array to search
+     * @param howMany - how many results to search for
+     * @return an array of the searched for indexes
+     * @throws Exception if the array length is shorter than 'howMany'
+     */
     public static int[] getMinValuesIndexes(double[] array, int howMany) throws Exception {
         if (howMany > array.length)
             throw new Exception(
@@ -45,6 +73,14 @@ public class Array {
         return minValuesIndexes;
     }
 
+    /**
+     * find the index of the minimum value of an array, and set the value at that
+     * index to the highest value possible, effectively removing it from future
+     * searches
+     * 
+     * @param array - the array to search
+     * @return an array of the searched for indexes
+     */
     public static int getMinValueIndex(double[] array) {
         int minValueIndex = 0;
 
