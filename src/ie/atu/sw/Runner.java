a7ee5dc6f9c510e817655c731f9738f7ec2311fd
Conor Dowdall <conordowdallmail@gmail.com>
2024-05-27 20:12:18 +0100

    move class with main method to package directory

---
+++ src/ie/atu/sw/Runner.java
@@ -0,0 +1,11 @@
+package ie.atu.sw;
+
+import ie.atu.sw.menu.MainMenu;
+
+public class Runner {
+    public static void main(String[] args) throws Exception {
+
+        new MainMenu().launchMenu();
+
+    }
+}
